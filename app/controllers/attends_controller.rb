24f70e4376d16fa34ec9c607883f0780b0b40f50
arakikeita <araki1728@gmail.com>
2023-03-07 16:44:40 +0900

    出欠削除機能実装

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -1,4 +1,7 @@
 class AttendsController < ApplicationController
+  before_action :authenticate_user!, only: [:create,:destroy]
+  before_action :set_attend,only: [:destroy]
+  before_action :move_to_index, only: [:destroy]
 
   def create
     attend = Attend.create(attend_params)
@@ -7,7 +10,7 @@ class AttendsController < ApplicationController
 
   def destroy
     @calender = Calender.find(params[:calender_id])
-    @attend = Attend.find(params[:id])
+    
     @attend.destroy
     redirect_to "/calenders/#{@attend.calender_id}"
   
@@ -17,6 +20,16 @@ class AttendsController < ApplicationController
     private
   def attend_params
   params.require(:attend).permit(:attendance_id).merge(user_id: current_user.id, calender_id: params[:calender_id])
+  end
+
+  def set_attend
+    @attend = Attend.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @attend.user_id
+      redirect_to action: :index
+    end
   end
 
 end
