f8db52cf060b312acd9629f5664a45c295487e60
arakikeita <araki1728@gmail.com>
2023-03-10 21:21:11 +0900

    申請一覧表示

--- app/controllers/user_teams_controller.rb
+++ app/controllers/user_teams_controller.rb
@@ -3,7 +3,7 @@ class UserTeamsController < ApplicationController
   def create
     @user_team = UserTeam.create(team_id: user_team_params[:team_id], user_id: user_team_params[:user_id])
     Apply.find(user_team_params[:apply_id]).destroy!
-    redirect_to team_applies_url(@user_team.team), notice:"「#{@user_team.user.username}」が、コミュニティ：#{@user_team.team.team_name}へ加入しました。"
+    redirect_to team_applies_url(@user_team.team), notice:"「#{@user_team.user.nickname}」が、コミュニティ：#{@user_team.team.team_name}へ加入しました。"
   end
 
   def destroy
